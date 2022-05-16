6ead3875730a08b82220934c5480486f87c4e2de
Franco Alejandro Alancay <francoalancayomg369@gmail.com>
2022-05-15 22:12:15 -0300

    actualizacion del html

--- src/main/java/ar/edu/unju/edm/model/Mes.java
+++ src/main/java/ar/edu/unju/edm/model/Mes.java
@@ -23,49 +23,49 @@ public class Mes {
     }else{
       if(num==2){
         aux="Febrero";
-      }else{
+    }else{
         if(num==3){
           aux="Marzo";
-        }else{
-          if(num==4){
-            aux="Abril";
-          }else{
-            if(num==5){
-              aux="Mayo";
-            }else{
-              if(num==6){
-                aux="Junio";
-              }else{
-                if(num==7){
-                  aux="Julio";
-                }else{
-                  if(num==8){
-                    aux="Agosto";
-                  }else{
-                    if(num==9){
-                      aux="Septiembre";
-                    }else{
-                      if(num==10){
-                        aux="Octubre";
-                      }else{
-                        if(num==11){
-                          aux="Noviembre";
-                        }else{
-                          if(num==12){
-                            aux="Diciembre";
-                          }else{
-                            aux="No existe ese mes";
-                          }
-                        }
+    }else{
+        if(num==4){
+         aux="Abril";
+    }else{
+        if(num==5){
+          aux="Mayo";
+    }else{
+        if(num==6){
+         aux="Junio";
+    }else{
+       if(num==7){
+          aux="Julio";
+    }else{
+       if(num==8){
+        aux="Agosto";
+    }else{
+       if(num==9){
+        aux="Septiembre";
+    }else{
+      if(num==10){
+        aux="Octubre";
+    }else{
+      if(num==11){
+        aux="Noviembre";
+    }else{
+      if(num==12){
+        aux="Diciembre";
+    }else{
+    aux="No existe ese mes";
+              }
+              }
                       }
                     }
                   }
                 }
-              }
-            }
-          }
-        }
-      }
+   }
+    }
+    }
+    }
+    }
     }
     return aux;
   }
